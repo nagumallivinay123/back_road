b6c51bcd62a812b7f66ae6c4bdd69e6ca2285308
Nagumalli Vinay <92999187+nagumallivinay123@users.noreply.github.com>
2024-06-01 10:34:48 +0530

    Update data.js

--- src/components/data.js
+++ src/components/data.js
@@ -12,7 +12,6 @@ export const pageLinks = [
 export const socialLinks = [
   { id: 1, href: "https://www.twitter.com", icon: "fab fa-facebook" },
   { id: 2, href: "https://www.twitter.com", icon: "fab fa-twitter" },
-  ,
   { id: 3, href: "https://www.twitter.com", icon: "fab fa-squarespace" },
 ];
 export const service = [
